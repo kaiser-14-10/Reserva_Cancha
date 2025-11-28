e84050712db9e28e673df0c3f0cdced9578e412b
David073471 <ds073471@gmail.com>
2025-11-27 23:24:10 -0300

    Update urls.py

--- arriendo_canchas/reservas/urls.py
+++ arriendo_canchas/reservas/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('reservar/<int:cancha_id>/', views.reservar, name='reservar'),
-    path('mis_reservas/', views.mis_reservas, name='mis_reservas'),
-    path('cancelar/<int:id>/', views.cancelar_reserva, name='cancelar'),
-    path('login/', views.login_usuario, name='login'),
-    path('logout/', views.logout_usuario, name='logout'),
-
+    path("", views.home, name="home"),
+    path("reservar/<int:cancha_id>/", views.reservar, name="reservar"),
+    path("mis-reservas/", views.mis_reservas, name="mis_reservas"),
+    path("cancelar/<int:id>/", views.cancelar, name="cancelar"),
+    path("calendario/", views.calendario, name="calendario"),
+    path("pago/", views.pago, name="pago"),
+    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
+    path("register/", views.register, name="register"),
 ]
