fa37b2c1ee89739a5c3f7ed3e897d1570f0a28a0
David073471 <ds073471@gmail.com>
2025-11-30 17:54:53 -0300

    Update urls.py

--- arriendo_canchas/reservas/urls.py
+++ arriendo_canchas/reservas/urls.py
@@ -8,12 +8,15 @@ urlpatterns = [
     path("mis-reservas/", views.mis_reservas, name="mis_reservas"),
     path("cancelar/<int:id>/", views.cancelar, name="cancelar"),
     path("calendario/", views.calendario, name="calendario"),
-    path("pago/<int:reserva_id>/", views.pago, name="pago"),
 
 
     path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
     path("register/", views.register, name="register"),
+    path('pago/<int:reserva_id>/', views.pago, name='pago'),
+    path('pago-exitoso/', views.pago_exitoso, name='pago_exitoso'),
+
+
 
 ]
 
