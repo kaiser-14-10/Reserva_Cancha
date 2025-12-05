c364c5998d50039a57c94c464a772c02353ea62c
David073471 <ds073471@gmail.com>
2025-12-05 17:11:06 -0300

    Update admin.py

--- arriendo_canchas/reservas/admin.py
+++ arriendo_canchas/reservas/admin.py
@@ -1,14 +1,24 @@
 from django.contrib import admin
-from .models import Cancha, Reserva, Log, FechasNoDisponibles
+from .models import Cancha, Categoria, Reserva, FechasNoDisponibles
 
-admin.site.register(Cancha)
-admin.site.register(Reserva)
-admin.site.register(Log)
+@admin.register(Cancha)
+class CanchaAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "categoria", "precio", "disponible")
+    list_filter = ("categoria", "disponible")
+
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ("nombre",)
+
+
+@admin.register(Reserva)
+class ReservaAdmin(admin.ModelAdmin):
+    list_display = ("cancha", "usuario", "fecha", "hora_inicio", "hora_fin", "estado")
+    list_filter = ("estado", "fecha")
+
 
 @admin.register(FechasNoDisponibles)
 class FechasNoDisponiblesAdmin(admin.ModelAdmin):
     list_display = ("cancha", "fecha_inicio", "fecha_fin", "motivo")
-    list_filter = ("cancha", "fecha_inicio", "fecha_fin")
-    search_fields = ("cancha__nombre", "motivo")
-
-
+    list_filter = ("cancha",)
