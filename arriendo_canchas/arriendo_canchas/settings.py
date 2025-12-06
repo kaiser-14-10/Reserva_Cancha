2d6c3f60dc4527b2ef113ca8472043772f976606
Luis-crts <luiscortes13531@gmail.com>
2025-12-06 00:28:20 -0300

    Agregar STATIC_ROOT para staticfiles

--- arriendo_canchas/arriendo_canchas/settings.py
+++ arriendo_canchas/arriendo_canchas/settings.py
@@ -75,9 +75,14 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [BASE_DIR / "static",]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
 MEDIA_ROOT = BASE_DIR / "media"
+
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
