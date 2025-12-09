c4758636f5c4f83f1642c4b31f3818a9fcd7f41b
David073471 <ds073471@gmail.com>
2025-12-09 17:42:52 -0300

    Add search fields to admin models

--- arriendo_canchas/reservas/admin.py
+++ arriendo_canchas/reservas/admin.py
@@ -5,20 +5,31 @@ from .models import Cancha, Categoria, Reserva, FechasNoDisponibles, Log
 class CanchaAdmin(admin.ModelAdmin):
     list_display = ("nombre", "categoria", "precio", "disponible")
     list_filter = ("categoria", "disponible")
+    search_fields = ("nombre",)
 
 
 @admin.register(Categoria)
 class CategoriaAdmin(admin.ModelAdmin):
     list_display = ("nombre",)
+    search_fields = ("nombre",)
 
 
 @admin.register(Reserva)
 class ReservaAdmin(admin.ModelAdmin):
     list_display = ("cancha", "usuario", "fecha", "hora_inicio", "hora_fin", "estado")
-    list_filter = ("estado", "fecha")
+    list_filter = ("estado", "fecha", "cancha")
+    search_fields = ("usuario__username", "cancha__nombre")
 
 
 @admin.register(FechasNoDisponibles)
 class FechasNoDisponiblesAdmin(admin.ModelAdmin):
     list_display = ("cancha", "fecha_inicio", "fecha_fin", "motivo")
-    list_filter = ("cancha",)
+    list_filter = ("cancha", "fecha_inicio")
+    search_fields = ("motivo",)
+
+
+@admin.register(Log)
+class LogAdmin(admin.ModelAdmin):
+    list_display = ("usuario", "accion", "fecha")
+    list_filter = ("fecha",)
+    search_fields = ("usuario__username", "accion")
